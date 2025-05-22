a228e11f264d9126af030d0b2e58458a8f3d3240
Victor Chicu <victor@aroundwire.com>
2025-05-22 09:14:31 +0300

    Initial commit

---
+++ src/main/java/app/awaytogo/gateway/AwaytogoGatewayApplication.java
@@ -0,0 +1,13 @@
+package app.awaytogo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwaytogoGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwaytogoGatewayApplication.class, args);
+    }
+
+}
