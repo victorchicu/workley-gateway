288a24d574cdda6573708140866acce6b7e5099c
Victor Chicu <victor@aroundwire.com>
2025-08-04 16:19:40 +0300

    Implement loading indicator for create resume button.

--- src/main/java/io/zumely/gateway/resume/interfaces/rest/PromptController.java
+++ src/main/java/io/zumely/gateway/resume/interfaces/rest/PromptController.java
@@ -7,6 +7,7 @@ import io.zumely.gateway.resume.application.prompt.PromptHandler;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -36,6 +37,7 @@ public class PromptController {
 
         return Mono.just(
                 ResponseEntity.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
                         .body(result)
         );
     }
