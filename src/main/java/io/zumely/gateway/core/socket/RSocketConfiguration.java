13a1dc0f2086ccce56471e4b4e878c99f0b6e052
Victor Chicu <victor@aroundwire.com>
2025-09-09 14:57:20 +0300

    Full stream FE<>BE completed

--- src/main/java/io/zumely/gateway/core/socket/RSocketConfiguration.java
+++ src/main/java/io/zumely/gateway/core/socket/RSocketConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Sinks;
 
 @Configuration
-public class SocketConfiguration {
+public class RSocketConfiguration {
     @Bean
     Sinks.Many<Message<String>> chatSink() {
         return Sinks.many().multicast().onBackpressureBuffer(1024, false);
